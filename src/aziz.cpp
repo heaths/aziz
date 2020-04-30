d1e7b70648a90b6c8efea65aa34af01bd9f4cb94
Heath Stewart <heaths@outlook.com>
2020-04-30 02:28:20 -0700

    Initial commit

---
+++ src/aziz.cpp
@@ -0,0 +1,12 @@
+﻿// Copyright 2020 Heath Stewart.
+// Licensed under the MIT License.See LICENSE.txt in the project root for license information.
+
+int wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_ HINSTANCE /* hPrevInstance */,
+    _In_ LPWSTR cmdline,
+    _In_ int /* wShow */
+)
+{
+    return ERROR_SUCCESS;
+}
---
+++ src/precomp.h
@@ -0,0 +1,9 @@
+// Copyright 2020 Heath Stewart.
+// Licensed under the MIT License.See LICENSE.txt in the project root for license information.
+
+#pragma once
+
+#include <Windows.h>
+
+#include <cpprest/http_client.h>
+#include <cpprest/json.h>
